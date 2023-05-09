ec3462c72eae535cda414de4d4be64fe448e6717
Tim Kelley <ctk@ncsu.edu>
2023-05-09 15:13:59 -0400

    Getting started

---
+++ src/N3Presults.jl
@@ -0,0 +1,21 @@
+"""
+N3Presults
+
+This module has the code you need to reproduce the results in my paper
+Newton's Method in Three Precisions
+
+"""
+module N3Presults
+
+export
+# Functions
+
+
+
+using PyPlot
+using LinearAlgebra
+using SIAMFANLEquations
+using SIAMFANLEquations.TestProblems
+using SIAMFANLEquations.Examples
+
+end #module
