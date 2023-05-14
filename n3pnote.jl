9ac47b0ac79d52597c77f46e6e57880a53683f1c
Tim Kelley <ctk@ncsu.edu>
2023-05-14 13:46:00 -0400

    fixing notebook woes

--- n3pnote.jl
+++ n3pnote.jl
@@ -2,6 +2,5 @@ using Printf
 Base.show(io::IO, f::Float64) = @printf(io, "%1.5e", f)
 Base.show(io::IO, f::Float32) = @printf(io, "%1.5e", f)
 Base.show(io::IO, f::Float16) = @printf(io, "%1.5e", f)
-using PyPlot
 push!(LOAD_PATH,"./src")
 using N3Presults
